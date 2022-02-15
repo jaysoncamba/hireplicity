478cf64d6a28aa718b48a8f1163eaaa581a068a1
jayson <mjmcamba@gmail.com>
2022-02-15 14:37:38 +0800

    shorter implementation

--- coding_excersises.rb
+++ coding_excersises.rb
@@ -2,20 +2,8 @@ class SortedArray < Array
 
   # Assumed that the array is already sorted.
 
-  # the idea here is to convert the array to hash and use index of the element as keys
-  # then get the key using value to get the index.
-  # using this route this would be faster if the collection is bigger and the number to search is large as well
-  # e.g array = (0..9000) and the number inputted is 8700
   def contains?(number)
-    # return !!(self & [number]).first
     return false unless number.is_a? Numeric
-    !!hash_value.key(number)
-  end
-  
-  private
-  # Memoized hash_value so we dont repeat initialization of the hash
-  # This would not be updated whenever we add or remove elements on the array
-  def hash_value
-    @hash_value ||= Hash[(0...self.size).zip(self)]
+    return !!(self & [number]).first
   end
 end
